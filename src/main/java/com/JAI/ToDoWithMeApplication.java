b9db91adaca0140d9f4956ef6a045790cabe90ac
Lunabell03 <sunnism03@gmail.com>
2024-11-19 18:00:04 +0900

    [init] 초기 세팅

---
+++ src/main/java/com/JAI/ToDoWithMeApplication.java
@@ -0,0 +1,13 @@
+package com.JAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoWithMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoWithMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JAI/ToDoWithMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoWithMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
