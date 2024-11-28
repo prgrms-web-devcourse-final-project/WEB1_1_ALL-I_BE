acdb951a8f8fd0df2f5ba6fc9465b25c35f90e6e
pyy2114 <pyy2114@naver.com>
2024-11-28 12:04:22 +0900

    feat: #3 DTO 클래스로 변환 및 Validation 추가

--- src/main/java/com/JAI/user/controller/request/UserLoginReq.java
+++ src/main/java/com/JAI/user/controller/request/UserLoginReq.java
@@ -1,7 +1,18 @@
 package com.JAI.user.controller.request;
 
-public record UserLoginReq(
-        String email,
-        String password
-) {
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class UserLoginReq {
+    @NotBlank(message = "이메일을 입력하셔야 합니다.")
+    String email;
+    @NotBlank(message = "비밀번호를 입력하셔야 합니다.")
+    String password;
 }
