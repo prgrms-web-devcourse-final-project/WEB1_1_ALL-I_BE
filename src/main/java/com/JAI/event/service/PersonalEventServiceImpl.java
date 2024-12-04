dd2f231441f43a936cbe52e9969055e706f17957
minjung <minjung0768@gmail.com>
2024-12-04 15:44:43 +0900

    bugfix : 추가 사용자가 본인의 카테고리만 사용할 수 있게 확인
    # Conflicts:
    #	src/main/java/com/JAI/event/service/PersonalEventServiceImpl.java

--- src/main/java/com/JAI/event/service/PersonalEventServiceImpl.java
+++ src/main/java/com/JAI/event/service/PersonalEventServiceImpl.java
@@ -2,6 +2,7 @@ package com.JAI.event.service;
 
 import com.JAI.alarm.service.AlarmService;
 import com.JAI.category.exception.CategoryNotFoundException;
+import com.JAI.category.exception.CategoryNotOwnerException;
 import com.JAI.category.service.CategoryService;
 import com.JAI.event.DTO.request.PersonalEventCreateReqDTO;
 import com.JAI.event.DTO.request.PersonalEventUpdateReqDTO;
@@ -47,7 +48,13 @@ public class PersonalEventServiceImpl implements PersonalEventService {
                 .orElseThrow(() -> new UserNotFoundException("해당 ID의 사용자를 찾을 수 없습니다.", personalEventCreateReqDTO.getUserId())));
 
         // categoryService를 통해 Category를 설정
-        personalEvent.setCategory(Optional.ofNullable(categoryService.getCategoryByCategoryId(personalEventCreateReqDTO.getCategoryId()))
+        personalEvent.setCategory(Optional.ofNullable(categoryService.getCategoryById(personalEventCreateReqDTO.getCategoryId()))
+                .map(category -> {
+                    if (category.getUser().getUserId().equals(userId) && category.getGroup() != null) {
+                        throw new CategoryNotOwnerException("다른 사용자의 카테고리를 사용할 수 없습니다.");
+                    }
+                    return category;
+                })
                 .orElseThrow(() -> new CategoryNotFoundException("해당 ID의 카테고리를 찾을 수 없습니다.", personalEventCreateReqDTO.getCategoryId())));
 
         personalEventRepository.save(personalEvent);
@@ -63,7 +70,7 @@ public class PersonalEventServiceImpl implements PersonalEventService {
         if (!personalEventId.equals(personalEventUpdateReqDTO.getPersonalEventId())) {
             throw new PersonalEventBadRequestException("잘못된 개인 일정 수정 요청입니다");
         }
-        
+
         // 기존 개인 일정 조회
         PersonalEvent existingEvent = personalEventRepository.findById(personalEventId)
                 .orElseThrow(() -> new PersonalEventNotFoundException("해당 ID의 개인 일정을 찾을 수 없습니다.", personalEventUpdateReqDTO.getPersonalEventId()));
@@ -77,11 +84,21 @@ public class PersonalEventServiceImpl implements PersonalEventService {
 
         // categoryService를 통해 Category를 설정
         Optional.ofNullable(personalEventUpdateReqDTO.getCategoryId())
-                .map(categoryService::getCategoryByCategoryId)
-                .ifPresent(category -> updatedPersonalEvent.setCategory(Optional.of(category)
-                        .orElseThrow(() -> new CategoryNotFoundException("해당 ID의 카테고리를 찾을 수 없습니다.", personalEventUpdateReqDTO.getCategoryId()))));
+                .map(categoryService::getCategoryById)
+                .map(category -> {
+                    if (!category.getUser().getUserId().equals(userId) && category.getGroup() != null) {
+                        throw new CategoryNotOwnerException("다른 사용자의 카테고리를 사용할 수 없습니다.");
+                    }
+                    updatedPersonalEvent.setCategory(category);
+                    return category;
+                })
+                .orElseThrow(() -> new CategoryNotFoundException("해당 ID의 카테고리를 찾을 수 없습니다.", personalEventUpdateReqDTO.getCategoryId()));
+
+
 
         personalEventRepository.save(updatedPersonalEvent);
+
+
 
         if (updatedPersonalEvent.getIsAlarmed()) {
             alarmService.updatePersonalEventAlarm(personalEventConverter.personalEventToPersonalEventDTO(updatedPersonalEvent));
@@ -100,12 +117,12 @@ public class PersonalEventServiceImpl implements PersonalEventService {
         if (!personalEventId.equals(existingEvent.getPersonalEventId())) {
             throw new PersonalEventBadRequestException("잘못된 개인 일정 수정 요청입니다");
         }
-        
+
         // 본인 외 다른 사용자의 개인 일정 삭제 요청 시 에러 처리
         if (!existingEvent.getUser().getUserId().equals(userId)) {
             throw new PersonalEventNotOwnerException("다른 사용자의 개인 일정을 삭제할 수 없습니다.");
         }
-        
+
         personalEventRepository.deleteById(personalEventId);
     }
 
@@ -114,7 +131,7 @@ public class PersonalEventServiceImpl implements PersonalEventService {
         // 사용자를 찾을 수 없을 시 사용
         Optional.ofNullable(userService.getUserById(userId))
                 .orElseThrow(() -> new UserNotFoundException("해당 ID의 사용자를 찾을 수 없습니다.", userId));
-        
+
         // 조회할 일정의 범위 설정
         LocalDate startDate = LocalDate.of(Integer.parseInt(year), Integer.parseInt(month), 1);
         LocalDate endDate = startDate.withDayOfMonth(startDate.lengthOfMonth());
