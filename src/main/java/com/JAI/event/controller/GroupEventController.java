9d408369600e712a8df6a890333bf297c10e2441
minjung <minjung0768@gmail.com>
2024-12-08 13:33:31 +0900

    bugfix : 수정 valid 에러 처리

--- src/main/java/com/JAI/event/controller/GroupEventController.java
+++ src/main/java/com/JAI/event/controller/GroupEventController.java
@@ -6,10 +6,13 @@ import com.JAI.event.DTO.response.GetGroupEventResDTO;
 import com.JAI.event.DTO.response.GroupEventResDTO;
 import com.JAI.event.service.GroupEventService;
 import com.JAI.global.controller.ApiResponse;
+import com.JAI.global.status.ErrorStatus;
 import com.JAI.user.service.dto.CustomUserDetails;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -36,12 +39,22 @@ public class GroupEventController {
     }
 
     @PostMapping("/{group_id}/events")
-    public ApiResponse<GroupEventResDTO> createGroupEvents(@PathVariable("group_id") UUID groupId, @Valid @RequestBody GroupEventCreateReqDTO groupEventCreateReqDTO, @AuthenticationPrincipal CustomUserDetails user) {
+    public ApiResponse<GroupEventResDTO> createGroupEvents(@PathVariable("group_id") UUID groupId, @Validated @RequestBody GroupEventCreateReqDTO groupEventCreateReqDTO, BindingResult bindingResult, @AuthenticationPrincipal CustomUserDetails user) {
+        if (bindingResult.hasErrors()) {
+            String errorMessage = bindingResult.getAllErrors().get(0).getDefaultMessage();
+            return ApiResponse.onFailure(ErrorStatus.BAD_REQUEST, errorMessage, null);
+        }
+
         return ApiResponse.onCreateSuccess(groupEventService.createGroupEvent(groupEventCreateReqDTO, groupId, user.getUserId()));
     }
 
     @PatchMapping("/{group_id}/events/{group_event_id}")
-    public ApiResponse<GroupEventResDTO> updateGroupEvents(@PathVariable("group_id") UUID groupId, @PathVariable("group_event_id") UUID groupEventId, @Valid @RequestBody GroupEventUpdateReqDTO groupEventUpdateReqDTO, @AuthenticationPrincipal CustomUserDetails user) {
+    public ApiResponse<GroupEventResDTO> updateGroupEvents(@PathVariable("group_id") UUID groupId, @PathVariable("group_event_id") UUID groupEventId, @Valid @RequestBody GroupEventUpdateReqDTO groupEventUpdateReqDTO, BindingResult bindingResult, @AuthenticationPrincipal CustomUserDetails user) {
+        if (bindingResult.hasErrors()) {
+            String errorMessage = bindingResult.getAllErrors().get(0).getDefaultMessage();
+            return ApiResponse.onFailure(ErrorStatus.BAD_REQUEST, errorMessage, null);
+        }
+
         return ApiResponse.onSuccess(groupEventService.updateGroupEvent(groupId, groupEventId, groupEventUpdateReqDTO, user.getUserId()));
     }
 
